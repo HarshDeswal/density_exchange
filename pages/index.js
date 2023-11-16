ed0d7af8804810e6359cf052a55b1c8a3188dba1
harshdeswal <harshdeswal03@yahoo.com>
2023-11-16 17:58:39 +0530

    Made card slider by framer motion &  react viz senor

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import EQbeats from '@/Components/EQBeats'
 import Footer from '@/Components/Footer'
 import AnimationOne from '@/Components/FirstAnimation'
-
+import CardSlider from '@/Components/CardSlider'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -10,6 +10,7 @@ export default function Home() {
     <>
      <AnimationOne/>
      <EQbeats/>
+     <CardSlider/>
      <Footer/>
     </>
   )
